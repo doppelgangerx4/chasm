a2fa42de69d5a86efcae159c074417553b5e2689
Kevin Chen <erf@standarddoubt.com>
2013-08-07 06:10:19 -0400

    Triage on linking issues in Chasm

--- Chasm/chasm/Assets/JumpTo.cs
+++ Chasm/chasm/Assets/JumpTo.cs
@@ -8,14 +8,14 @@ public class JumpTo : MonoBehaviour {
 	
 public GameObject player; //the player
 public bool isMouseOver = false; //is mouse over? (sometimes you don't need a comment I guess!)
-public PlayerScript PlayerScript; //the player's scripting.
+public PlayerScript playerScript; //the player's scripting.
 public GameObject mySelf;
 	
 	
 	// Use this for initialization
 	void Start () {
 		mySelf = this.gameObject;
-	PlayerScript = player.GetComponent<PlayerScript>();
+	playerScript = player.GetComponent<PlayerScript>();
 		
 	}
 	
@@ -25,7 +25,7 @@ public GameObject mySelf;
 		if(isMouseOver == true && Input.GetMouseButtonDown(0)) //if player clicks on me
 			{
 
-			PlayerScript.JumpTo(gameObject.transform); //call the player's JumpTo() to make them jump to me
+			playerScript.JumpTo(gameObject.transform); //call the player's JumpTo() to make them jump to me
 	
 			}
 	
